cd5d3eb0cbc4adc7165ea1cd0998f03ace469d85
SShipon <nxshipon@gmail.com>
2023-12-10 02:09:51 -0800

    :apple: code solved

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 var jwt = require('jsonwebtoken');
 require('dotenv').config();
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
